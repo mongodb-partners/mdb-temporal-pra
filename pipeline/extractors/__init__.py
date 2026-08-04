c91c17d758df66b90a74e67acfd91cb0b38dd12f
Suresh Ramappa <suresha.ram@mongodb.com>
2026-08-04 22:03:58 +0530

    Publish static docs site for GitHub Pages

--- pipeline/extractors/__init__.py
+++
@@ -1,6 +0,0 @@
-"""File-type extractor factory (markdown / pdf / csv / text)."""
-
-from .base import Extractor, RawChunk
-from .factory import get_extractor
-
-__all__ = ["Extractor", "RawChunk", "get_extractor"]
--- pipeline/workflows/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Temporal workflows for the ingest pipeline."""
-
-from .backfill_workflow import BackfillWorkflow
-from .ingest_workflow import IngestWorkflow
-
-ALL_WORKFLOWS = [IngestWorkflow, BackfillWorkflow]
